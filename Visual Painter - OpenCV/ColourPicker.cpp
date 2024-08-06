593041fca20051c9d8b99492451233660b1693e9
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2024-08-06 23:48:28 +0530

    Update ColourPicker.cpp

--- Visual Painter - OpenCV/ColourPicker.cpp
+++ Visual Painter - OpenCV/ColourPicker.cpp
@@ -1,62 +1,67 @@
-#include<opencv2/imgcodecs.hpp>
-#include<opencv2/highgui.hpp>
-#include<opencv2/imgproc.hpp>
-#include<iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
 
 using namespace std;
 using namespace cv;
 
-////////// COLOR DETECTION //////////
-
-Mat imgHSV, imgMask; 
-
-
+Mat imgHSV, imgMask;
 
 int hmin = 0, smin = 0, vmin = 0;
-int hmax = 255, smax = 255, vmax = 255;
+int hmax = 179, smax = 255, vmax = 255;
 
 VideoCapture cap(0);
 Mat img;
 
+void onTrackbarChange(int, void*) {
+    // This function is a callback for trackbar events, it will be called whenever trackbar values change
+}
 
 int main() {
+    if (!cap.isOpened()) {
+        cerr << "Error: Camera not opened!" << endl;
+        return -1;
+    }
 
-	
+    // Create a Window for trackbars
+    namedWindow("Trackbars", WINDOW_AUTOSIZE);
 
-	//Creating a Window for trackbars
-	namedWindow("Trackbars", (640, 200));
+    // Create trackbars
+    createTrackbar("Hue Min", "Trackbars", &hmin, 179, onTrackbarChange);
+    createTrackbar("Hue Max", "Trackbars", &hmax, 179, onTrackbarChange);
+    createTrackbar("Sat Min", "Trackbars", &smin, 255, onTrackbarChange);
+    createTrackbar("Sat Max", "Trackbars", &smax, 255, onTrackbarChange);
+    createTrackbar("Val Min", "Trackbars", &vmin, 255, onTrackbarChange);
+    createTrackbar("Val Max", "Trackbars", &vmax, 255, onTrackbarChange);
 
-	//creating taskbars
-	createTrackbar("Hue Min", "Trackbars", &hmin, 179);
-	createTrackbar("Hue Max", "Trackbars", &hmax, 179);
-	createTrackbar("Sat Min", "Trackbars", &smin, 255);
-	createTrackbar("Sat Max", "Trackbars", &smax, 255);
-	createTrackbar("Val Min", "Trackbars", &vmin, 255);
-	createTrackbar("Val Max", "Trackbars", &vmax, 255);
+    while (true) {
+        if (!cap.read(img)) {
+            cerr << "Error: Failed to capture image!" << endl;
+            break;
+        }
 
+        // Convert to HSV color space
+        cvtColor(img, imgHSV, COLOR_BGR2HSV);
 
-	while (true)
-	{
+        // Set the range of color to detect
+        Scalar lower(hmin, smin, vmin);
+        Scalar upper(hmax, smax, vmax);
 
-		cap.read(img);
+        // Threshold the HSV image to get the color within the range
+        inRange(imgHSV, lower, upper, imgMask);
 
+        // Display the results
+        imshow("Image", img);
+        imshow("Mask", imgMask);
 
-		//Converting to HSV color
-		cvtColor(img, imgHSV, COLOR_BGR2HSV);
+        // Exit on 'ESC' key
+        if (waitKey(1) == 27) {
+            break;
+        }
+    }
 
-		// func to detect color-> inRange
-		// lower and upper limits have been set for the rsnge of colour 
-		Scalar lower(hmin, smin, vmin);
-		Scalar upper(hmax, smax, vmax);
-		inRange(imgHSV, lower, upper, imgMask);
-
-		cout << hmin << "," << smin << "," << vmin << "," << hmax << "," << smax << "," << vmax << endl;
-		
-		imshow("image", img);
-		//imshow("image HSV", imgHSV);
-		imshow("Mask", imgMask);
-
-		waitKey(1);
-	}
-
+    cap.release();
+    destroyAllWindows();
+    return 0;
 }
